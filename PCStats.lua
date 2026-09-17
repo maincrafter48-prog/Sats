d0e1f80e3854194e219c37395c9df4964b8b298c
maincrafter48-prog <maincrafter48@gmail.com>
2026-09-17 11:36:00 +0300

    Update PCStats.lua

--- PCStats.lua
+++ PCStats.lua
@@ -1,9 +1,9 @@
 script_name("PC Stats")
 script_description("Statistika personazha | Arizona PC | by Marco_Santiago (PC port)")
 script_author("Marco_Santiago")
-script_version("1.3.4")
+script_version("1.3.6")
 
-local SCRIPT_VER = "1.8.3"
+local SCRIPT_VER = "1.8.6"
 
 -- имя чат-команды, зарегистрированной сейчас (для перерегистрации при смене)
 local _registeredMenuCmd = nil
